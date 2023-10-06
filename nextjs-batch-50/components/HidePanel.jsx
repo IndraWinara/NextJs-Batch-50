6e2d001d1ccf90128676dcad3712a1ed1c488c2b
IndraWinara <winaraindra@gmail.com>
2023-10-06 22:09:05 +0700

    tugas-05-Layout

--- nextjs-batch-50/components/HidePanel.jsx
+++ nextjs-batch-50/components/HidePanel.jsx
@@ -1,8 +1,56 @@
+import { Button, Drawer, DrawerBody, DrawerContent, DrawerHeader, DrawerOverlay } from '@chakra-ui/react'
 import React from 'react'
+import { useDisclosure } from '@chakra-ui/react'
+import MenuIcon from '@mui/icons-material/Menu';
+import GroupIcon from '@mui/icons-material/Group';
+import SpeakerPhoneIcon from '@mui/icons-material/SpeakerPhone';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import SettingsIcon from '@mui/icons-material/Settings';
+import CallIcon from '@mui/icons-material/Call';
 
 const HidePanel = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
   return (
-    <div>HidePanel</div>
+    <>
+      <Button onClick={onOpen}>
+        <MenuIcon sx={{ fontSize: 28 }} className='mx-2 text-gray-400 cursor-pointer' />
+      </Button>
+      <Drawer placement='left' onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth='1px'>User Account</DrawerHeader>
+          <DrawerBody>
+            <div className='text-[15px] font-bold h-[300px]'>
+              <div className='flex items-center gap-5 p-2 hover:px-3 duration-300 hover:bg-gray-300 cursor-pointer'>
+                <GroupIcon />
+                <p className=''>New Group</p>
+              </div>
+              <div className='flex items-center gap-5 p-2 hover:px-3 duration-300 hover:bg-gray-300 cursor-pointer'>
+                <SpeakerPhoneIcon className='rotate-90' />
+                <p className=''>New Chanel</p>
+              </div>
+              <div className='flex items-center gap-5 p-2 hover:px-3 duration-300 hover:bg-gray-300 cursor-pointer'>
+                <AccountCircleIcon />
+                <p className=''>Contacts</p>
+              </div>
+              <div className='flex items-center gap-5 p-2 hover:px-3 duration-300 hover:bg-gray-300 cursor-pointer'>
+                <CallIcon />
+                <p className=''>Calls</p>
+              </div>
+              <div className='flex items-center gap-5 p-2 hover:px-3 duration-300 hover:bg-gray-300 cursor-pointer'>
+                <BookmarkIcon />
+                <p className=''>Saved Messages</p>
+              </div>
+              <div className='flex items-center gap-5 p-2 hover:px-3 duration-300 hover:bg-gray-300 cursor-pointer'>
+                <SettingsIcon />
+                <p className=''>Settings</p>
+              </div>
+            </div>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
   )
 }
 
--- nextjs-batch-50/components/SidePanel.jsx
+++ nextjs-batch-50/components/SidePanel.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import MenuIcon from '@mui/icons-material/Menu';
 import Group from './GroupSection/Group';
+import HidePanel from './HidePanel';
+
 
 const SidePanel = () => {
+
   return (
     <div className='w-[250px] p-2 h-screen bg-gray-100 border-r-[1px] border-gray-300 fixed'>
       <div className='mt-2 flex items-center'>
-        <MenuIcon sx={{ fontSize: 28 }} className='mx-2 text-gray-400 cursor-pointer' />
+        <>
+          <HidePanel />
+        </>
         <input placeholder='Search...' className='p-2 outline-gray-300 focus:bg-gray-100 rounded-full bg-gray-300 text-sm' />
       </div>
       {/* group */}
